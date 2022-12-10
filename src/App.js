a200953aac67b3c67d5bd21c45d8667968d8fdbc
Valentine Wabwire <valentine.w.maita@gmail.com>
2022-12-10 12:24:59 +0300

    set voice commands

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import "./App.css";
 
 function App() {
+  /* An array of objects that contains the commands that the user can say to the app. */
+  const commands = [
+    {
+      command: "reset",
+      callback: ({ resetTranscript }) => {
+        resetTranscript();
+      },
+    },
+    {
+      command: "clear",
+      callback: ({ resetTranscript }) => resetTranscript(),
+    },
+    {
+      command: "open *",
+      callback: (site) => {
+        window.open("http://" + site);
+      },
+    },
+    {
+      command: "increase text size",
+      callback: () => {
+        document.getElementById("content").style.fontSize = "22px";
+      },
+    },
+    {
+      command: "decrease text size",
+      callback: () => {
+        document.getElementById("content").style.fontSize = "16px";
+      },
+    },
+    {
+      command: "change text colour to *",
+      callback: (color) => {
+        document.getElementById("content").style.color = color;
+      },
+    },
+  ];
+
+  /* A function that is used to start the speech recognition. */
+  SpeechRecognition.startListening({ continuous: true, language: "en-US" });
+  const {
+    transcript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+  } = useSpeechRecognition({ commands });
+
+  if (!isMicrophoneAvailable) {
+    // Render some fallback content
+    alert("Allow React Assistance to access Microphone 🥺🥺");
+    return null;
+  }
+
+  /* Checking if the browser supports speech recognition. If it does not, it returns null. */
+  if (!browserSupportsSpeechRecognition) {
+    alert("Sorry 😭😭😭 Ýour browser does not support this feature");
+    return null;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /* The main component of the app. It is the container for the app. */
+    <div className="container">
+      <div className="nav">
+        <h2>Please Say Something 🥺🥺</h2>
+      </div>
+      <div id="content">{transcript}</div>
     </div>
   );
 }
